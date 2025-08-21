d714b99e39928871311525ebe6462a9adba1be49
KAdithyaReddy <adithyareddi23@gmail.com>
2025-08-21 23:50:41 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
+  base: '/ArcIT/',  // 👈 Replace <repo-name> with your GitHub repo name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
